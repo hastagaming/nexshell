57fe54bb1b6dcc6dd153fc397a7c184c358008f2
hastagaming <rip.hastagaming@gmail.com>
2026-07-31 17:59:39 +0700

    Add missing java.io.File import to TerminalSessionHolder

--- app/src/main/java/com/nexshell/terminal/TerminalSessionHolder.kt
+++ app/src/main/java/com/nexshell/terminal/TerminalSessionHolder.kt
@@ -1,6 +1,7 @@
 package com.nexshell.terminal
 
 import com.nexshell.core.Workspace
+import java.io.File
 import com.nexshell.core.WorkspaceProperties
 import com.termux.terminal.TerminalSession
 import java.util.UUID
